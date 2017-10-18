09c9ab34e5c534f68d70831df9c75899bf133ef4
gillky <31664632+gillky@users.noreply.github.com>
2017-10-18 19:19:04 -0400

    Update insertBets.php
    update insert bets

--- insertBets.php
+++ insertBets.php
@@ -1,25 +1,31 @@
 <?php session_start();
-$_SESSION["qb"];
-$_SESSION["rb"];
-$_SESSION["wr"];
+$_SESSION['qb'];
+$_SESSION['rb'];
+$_SESSION['wr'];
 
 ?>
 
 <?php
 
-$qb = $_GET["qb"];
-$wr = $_GET["wr"];
-$rb = $_GET["rb"];
+$qb = $_GET['qb'];
+$rb = $_GET['rb'];
+$wr = $_GET['wr'];
+
 
 require 'database1.php';
 $conn    = Connect();
-$bet    = $conn->real_escape_string($_POST['bet_amount']);
+$bet    = $_POST['bet_amount'];
 
-$query   = "INSERT into user_bets (screenname,bet_amount,qb_pick,rb_pick,wr_pick) VALUES('" . "dc376" . "','" . $bet . "','" . $qb . "','" . $rb . "','" . $wr . "')";
+
+$query   = "INSERT into user_bets (screenname,bet_amount,qb_pick,rb_pick,wr_pick) VALUES('" . "dc376" . "','" . $bet . "','" . $_SESSION["qb"] . "','" . $_SESSION["rb"] . "','" . $_SESSION["wr"] . "')";
+
 $success = $conn->query($query);
  
 
-echo" whats good";
+echo "<br><br>The QB is ". $_SESSION['qb'];
+echo "<br><br>The RB is ". $_SESSION['rb'];
+echo "<br><br>The WR is ". $_SESSION['wr'];
+echo "<br><br>The bet amount is ". $bet;
 
 
 
@@ -28,3 +34,5 @@ echo" whats good";
 $conn->close();
  
 ?>
+
+
