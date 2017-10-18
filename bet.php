e488d4eb8a3aeadf762a2252fbd6c4ec8d313d7b
dc376 <dc376@njit.edu>
2017-10-18 17:16:28 -0400

    work in progress

---
+++ bet.php
@@ -0,0 +1,32 @@
+<?php
+
+
+// retrieve the form data by using the element's name attributes value as key 
+$qb = $_GET['qb']; 
+$rb = $_GET['rb']; 
+$wr = $_GET['wr'];
+
+print "<html>";
+print" <head>";
+print "<h><b>$qb</b></h>";
+
+print"</head>";
+print "<br><br><br><br>";
+print "<html>";
+print" <head>";
+print "<h><b>$rb</b></h>";
+print"</head>";
+print "<br><br><br><br>";
+print "<html>";
+print" <head>";
+print "<h><b>$wr</b></h>";
+print"</head>";
+
+
+print"</html>";
+ ?>
+
+<input type = text name = "bet amount" id = "bet 1" placeholder="Enter Value: " >Bet1<br>
+
+
+
