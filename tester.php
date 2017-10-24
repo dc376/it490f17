64d8fccee5bae71d706497de8cdad06baf489b0b
dc376 <dc376@njit.edu>
2017-10-24 11:19:29 -0400

    changed write file location

--- tester.php
+++ tester.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php
 <?php
+$ch = curl_init("https://api.sportradar.us/nfl-t1/2017/reg/7/leaders.json?api_key=n7xdgystegrsbwp5sxn3cspq");
 
-$ch = curl_init("https://api.sportradar.us/nfl-t1/2017/reg/6/leaders.json?api_key=n7xdgystegrsbwp5sxn3cspq");
-$fp = fopen("output2.txt", "w");
+
+$fp = fopen("/home/dean/git/jawn/output2.txt", "w+");
+
 
 curl_setopt($ch, CURLOPT_FILE, $fp);
+
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
 curl_exec($ch);
 curl_close($ch);
 fclose($fp);
 ?>
-
