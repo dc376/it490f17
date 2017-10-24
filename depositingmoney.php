aec8388837f29dc5584ed083fa3bcaa31b8bcd02
dc376 <dc376@njit.edu>
2017-10-24 11:17:23 -0400

    grabs current balance correctly now

--- depositingmoney.php
+++ depositingmoney.php
@@ -5,27 +5,19 @@
 <?php
 include "database.php";
 session_start();
-
-
 $con = Connect();
 $getUserBalance = "SELECT balance AS var FROM users WHERE screenname = 'dc376'";
-$userBalanceQuery - mysqli_query($con, $getUserBalance);
-
+$userBalanceQuery = mysqli_query($con, $getUserBalance);
 $balanceResult = $userBalanceQuery->fetch_object()->var;
-echo $balanceResult;
+echo "current balance: " . $balanceResult;
 
 
 $deposit = $_GET['amount'];
 $_SESSION['amount'] = $deposit;
 
-
-
+$deposit = $deposit + $balanceResult;
 $newBalance = "UPDATE users set balance = '$deposit' where screenname = 'dc376'";
 $success = $con->query($newBalance);
-
-
-
-
 ?>
 
 <h2> Deposited Amount </h2>
@@ -35,8 +27,4 @@ $success = $con->query($newBalance);
 </style>
 </body>
 </html>
-
-
-
-
 
