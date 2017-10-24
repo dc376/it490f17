722bd2d3dfc04c4d5bc2b0e2565415edbdbb6586
fw55 <31710371+fw55@users.noreply.github.com>
2017-10-24 10:11:06 -0400

    Create depositingmoney.php

---
+++ depositingmoney.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+include "database.php";
+session_start();
+
+
+$con = Connect();
+$getUserBalance = "SELECT balance AS var FROM users WHERE screenname = 'dc376'";
+$userBalanceQuery - mysqli_query($con, $getUserBalance);
+
+$balanceResult = $userBalanceQuery->fetch_object()->var;
+echo $balanceResult;
+
+
+$deposit = $_GET['amount'];
+$_SESSION['amount'] = $deposit;
+
+
+
+$newBalance = "UPDATE users set balance = '$deposit' where screenname = 'dc376'";
+$success = $con->query($newBalance);
+
+
+
+
+?>
+
+<h2> Deposited Amount </h2>
+<p> Deposited amount = <?=$_SESSION['amount']?></p>
+
+
+</style>
+</body>
+</html>
+
+
+
+
+
